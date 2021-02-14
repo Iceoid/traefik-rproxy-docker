de009055d34e6e0fb5e8d6ff0bd322389373011c
cj <charles.jobin@gmail.com>
2021-02-14 03:01:42 +0000

    added prompts before rm

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,8 @@ sudo docker stop traefik traefik_website
 sudo docker rm traefik traefik_website
 sudo docker network rm traefik_net
 
-rm -f .env
-rm -f .htpasswd
+rm -f -i website/html/.
+
+rm -f -i .env
+rm -f -i .htpasswd
 rm -f -i conf/acme.conf

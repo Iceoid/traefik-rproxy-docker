e6d2d858aad47bb0423b4a7ddc03021fcc642a3e
cj <charles.jobin@gmail.com>
2021-02-14 01:32:24 +0000

    another syntax fix on the bash script

--- prep.sh
+++ prep.sh
@@ -11,9 +11,8 @@ echo "DOMAINNAME=${DOMAINNAME}" >> .env
 echo "CF_API_EMAIL=" >> .env
 echo "CF_API_KEY=" >> .env
 
-echo "You need to enter your cloudflare credentials in the .env file."
-
 touch .htpasswd
+echo "executing apt update && apt install apache2-utils...enter sudo password:"
 sudo apt update -y && sudo apt install apache2-utils -y
 
 echo "Enter a username:"
@@ -22,8 +21,9 @@ read USERNAME
 echo "Enter a password:"
 read PASSWORD
 
-USER_HASHEDPWD=${htpasswd -nb $USERNAME $PASSWORD}
+USER_HASHEDPWD=$(htpasswd -nb ${USERNAME} ${PASSWORD})
 echo "${USER_HASHEDPWD}" >> .htpasswd
 chmod 600 .htpasswd
 
 echo "Done!"
+echo "You still need to enter your cloudflare credentials in the .env file."
